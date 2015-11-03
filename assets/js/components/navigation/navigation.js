2e847813222dac4d3d35d04e22ccd8a7deb7d3a8
Jean-Philippe Bidegain <jp@bidega.in>
2015-11-03 10:32:44 +0100

    Corrected className

--- assets/js/components/navigation/navigation.js
+++ assets/js/components/navigation/navigation.js
@@ -5,7 +5,7 @@ import styles from './navigation.css'
 class Navigation extends Component {
   render() {
     return (
-      <ul className="{styles.common}">
+      <ul className={styles.common}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/search">Search a beer</Link></li>
       </ul>
