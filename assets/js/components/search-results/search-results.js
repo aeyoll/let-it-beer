2ebcb8d41bf4bc43fd51ba1dbe3db23470b924cc
Jean-Philippe Bidegain <jp@bidega.in>
2015-11-08 23:11:07 +0100

    scss -> css

--- assets/js/components/search-results/search-results.js
+++ assets/js/components/search-results/search-results.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import SearchResultsItem from '../search-results-item/search-results-item'
+import styles from './search-results.scss'
 
 class SearchResults extends Component {
   render() {
