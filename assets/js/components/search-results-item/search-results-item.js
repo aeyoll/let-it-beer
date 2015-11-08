4181f2e5f09be2d3dd3514983b823e22c32106b0
Jean-Philippe Bidegain <jp@bidega.in>
2015-11-08 11:29:38 +0100

    Handle saving new beers

--- assets/js/components/search-results-item/search-results-item.js
+++ assets/js/components/search-results-item/search-results-item.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import $ from 'jquery'
 
 class SearchResultsItem extends Component {
+  constructor() {
+    super();
+
+    this.handleBeerAdd = this.handleBeerAdd.bind(this);
+  }
+
+  handleBeerAdd() {
+    $.post('/api/beers/', this.props.beer);
+  }
+
   render() {
     return (
-      <div>{this.props.beer.name}</div>
+      <div>{this.props.beer.name} <button onClick={this.handleBeerAdd}>Add to my beers</button></div>
     );
   }
 }
