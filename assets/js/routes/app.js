e33beda68183eb66bb25929a3ba93f896a80674d
Jean-Philippe Bidegain <jp@bidega.in>
2015-11-14 16:32:55 +0100

    Added a title component

--- assets/js/routes/app.js
+++ assets/js/routes/app.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
 import Navigation from '../components/navigation/navigation'
+import Title from '../components/title/title'
 
 // Styles
 import styles from '../../scss/app.scss'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.props = {
+      title: 'Let it beer!'
+    }
+  }
+
   render() {
     return (
       <div className={styles.container}>
+        <Title title={this.props.title}></Title>
         <Navigation></Navigation>
 
         <h1 className={styles.title}>Let it beer!</h1>
