f3fc60b53f7ef9ef80a1f8398f65fd6c429d6726
Jean-Philippe Bidegain <jp@bidega.in>
2015-11-14 19:34:13 +0100

    Added the ripple effect

--- assets/js/components/button/button.js
+++ assets/js/components/button/button.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react'
+import Ripple from 'react-toolbox/lib/ripple';
 import styles from './button.scss'
 
 class Button extends Component {
   static propTypes = {
     className: React.PropTypes.string,
     disabled: React.PropTypes.bool,
+    kind: React.PropTypes.string,
     label: React.PropTypes.string,
     large: React.PropTypes.bool,
-    loading: React.PropTypes.bool
+    loading: React.PropTypes.bool,
+    ripple: React.PropTypes.bool
   }
 
   static defaultProps = {
+    kind: 'primary',
     loading: false,
-    large: false
+    large: false,
+    ripple: true
+  }
+
+  constructor () {
+    super()
+  }
+
+  handleMouseDown = (event) => {
+    event.preventDefault()
+
+    if (this.refs.ripple) {
+      this.refs.ripple.start(event)
+    }
+
+    if (this.props.onMouseDown) {
+      this.props.onMouseDown(event)
+    }
   }
 
   render() {
-    let className = ''
+    let className = styles[this.props.kind]
 
-    const {loading, large, label, ...others} = this.props;
+    const {kind, loading, large, label, ripple, ...others} = this.props;
 
     if (large) {
       className += ` ${styles.large}`;
@@ -29,7 +50,9 @@ class Button extends Component {
         {...others}
         className={className}
         disabled={this.props.disabled || this.props.loading}
+        onMouseDown={this.handleMouseDown}
       >
+        { ripple ? <Ripple ref='ripple' loading={loading}/> : null }
         { label ? <abbr className={styles.label}>{label}</abbr> : null }
       </button>
     )
