cfda201066ecab3eb17a4bc2d2262e176e560326
Jean-Philippe Bidegain <jp@bidega.in>
2015-11-15 17:10:59 +0100

    Beer list styling

--- assets/js/components/beer-list-item/beer-list-item.js
+++ assets/js/components/beer-list-item/beer-list-item.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
+import Button from '../button/button'
+
+import styles from './beer-list-item.scss'
+
 class BeerListItem extends Component {
   render() {
     return (
-      <div><Link to={`/beer/${this.props.beer.id}/`}>{this.props.beer.name}</Link></div>
+      <div className={styles.common}>
+        <Link className={styles.infos} to={`/beer/${this.props.beer.id}/`}>
+          <div className={styles.title}>{this.props.beer.name}</div>
+          <div className={styles.subtitle}>{this.props.beer.style}</div>
+        </Link>
+
+        <div className={styles.actions}>
+          <Button label="Delete"></Button>
+        </div>
+      </div>
     )
   }
 }
