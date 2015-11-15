4669dbf9de3265712ff6590f834e7ce393f3ec88
Jean-Philippe Bidegain <jp@bidega.in>
2015-11-15 09:17:52 +0100

    Added a link to the search page from the home

--- assets/js/routes/home.js
+++ assets/js/routes/home.js
@@ -2,15 +2,23 @@ import React, { Component } from 'react'
 import Button from '../components/button/button'
 
 class Home extends Component {
+  constructor() {
+    super()
+  }
+
   componentDidMount() {
     this.props.actions.updateTitle('Let it beer!')
+  }
+
+  goToSearch = () => {
+    this.props.history.pushState(null, `/search/`);
   }
 
   render() {
     return (
       <div>
         <img src="/assets/images/home.png" alt="Let it beer!" />
-        <Button label="Search a beer" large></Button>
+        <Button label="Search a beer" onMouseDown={this.goToSearch} large></Button>
       </div>
     )
   }
