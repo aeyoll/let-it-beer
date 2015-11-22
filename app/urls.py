9a20329f722655398311631cad5d6ff2d7d19242
Jean-Philippe Bidegain <jp@bidega.in>
2015-11-22 11:20:11 +0100

    Added jwt route

--- app/urls.py
+++ app/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api/', include('api.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='app.html'), name='app'),
     url(r'^.*/$', TemplateView.as_view(template_name='app.html'), name='app'),
